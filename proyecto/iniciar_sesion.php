15b2ef9fb66537694f23400c2b60ded3231cb2ea
Fernandorado22 <98455338+Fernandorado22@users.noreply.github.com>
2023-12-12 20:17:07 +0100

    Add files via upload

---
+++ proyecto/iniciar_sesion.php
@@ -0,0 +1,29 @@
+<html>   
+   <head>
+    <meta charset = "UTF-8">
+    <meta http-equiv = "X-UA-Compatible" content = "IE-edge, chrome = 1">
+    <meta name = "viewport" content = "width = device-width,initial-scale = 1.0">
+    <link rel = "stylesheet" type = "text/css" href = "/css/iniciosesion.css">
+    <title>Inventario - Inicio</title>
+    </head>
+    
+        <body>
+    
+            <main>
+    <div id="form">
+    <h1>Iniciar sesión</h1>
+    <form action = "/bd/compruebaUsuario.php" method = "POST">
+    <label for = "correo">Correo electrónico:</label>
+    <input type = "email" id = "correo" name = "correo" required>
+    </br /></br />
+    <label for = "contrasena">Contraseña:</label>
+    <input type = "password" id="passw" name = "passw" required>
+    </br /></br />
+    <input type = "submit" value="Iniciar sesión" >
+    </form>
+    <br>
+    <br>
+    <br>
+    </main>
+    </body>
+</html>
