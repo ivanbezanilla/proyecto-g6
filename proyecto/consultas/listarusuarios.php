590b986319347c6639ade3da2ba7da2f24794fcf
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-13 18:45:52 +0100

    modified:   proyecto/consultas/listarusuarios.php

--- proyecto/consultas/listarusuarios.php
+++ proyecto/consultas/listarusuarios.php
@@ -38,8 +38,8 @@ $personas = $sentencia->fetchAll(PDO::FETCH_OBJ);
 				<td><?php echo $proyecto->email ?></td>
                 <td><?php echo $proyecto->pass ?></td>
                 <td><?php echo $proyecto->tipo ?></td>
-				<td><a href="<?php echo "./consultas/editar.php?id=" . $proyecto->id?>">Editar</a></td>
-				<td><a href="<?php echo "./consultas/eliminar.php?id=" . $proyecto->id?>">Eliminar</a></td>
+				<td><a href="<?php echo "/editar.php?id=" . $proyecto->id?>">Editar</a></td>
+				<td><a href="<?php echo "/eliminar.php?id=" . $proyecto->id?>">Eliminar</a></td>
 			</tr>
 			<?php } ?>
 		</tbody>
