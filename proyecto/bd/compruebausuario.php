c9c96645639ee4615ab7e0d4d3e6f228d46eb7de
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-13 19:33:42 +0100

    modified:   proyecto/bd/compruebausuario.php 	modified:   proyecto/principal.php

--- proyecto/bd/compruebausuario.php
+++ proyecto/bd/compruebausuario.php
@@ -1,30 +1,28 @@
 <?php
-date_default_timezone_set('Europe/Madrid');
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $usuario = $_POST["correo"];
-    $password = $_POST["passw"];
-    echo "usuario: $usuario <br>";
-    echo "password: $password <br>";
-
-    // Conexión a la base de datos usando PDO
-
-    session_start();
-    include_once 'base_de_datos.php';
-
-    try {
-        // Consulta SQL para buscar el usuario
-        $sql = "SELECT * FROM usuario WHERE email = :usuario AND pass = :password";
-        $stmt = $base_de_datos->prepare($sql);
-        $stmt->bindParam(':usuario', $usuario);
-        $stmt->bindParam(':password', $password);
-        $stmt->execute();
-
-        $root_path = $_SERVER['DOCUMENT_ROOT'] . '/';
-        header("location: /proyecto-g6/proyecto/principal.php");
-        exit;
-    } catch(PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
+session_start();
+ 
+include "base_de_datos.php";
+ 
+$username = $_POST['correo'];
+$password = $_POST['passw'];
+ 
+$consulta = $conxBaseDatos->prepare("SELECT * FROM usuario WHERE email = :username AND pass = :password");
+$consulta->bindParam(':username', $username);
+$consulta->bindParam(':password', $password);
+$consulta->execute();
+ 
+if ($consulta->rowCount() == 1) {
+ 
+    $row = $consulta->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['usuario'] = $row['nombre'];
+    $_SESSION['contrasena'] = $row['contrasena'];
+ 
+    header("Location: administracion.php");
+    exit();
+ 
+} else {
+    echo "Usuario o contraseña incorrectos";
+    header("Location: index.php");
+    exit();
 }
 ?>
