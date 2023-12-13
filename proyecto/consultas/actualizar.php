0a1a7232203f8a44513fbb9702789febf412d58a
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-13 19:00:55 +0100

    modified:   proyecto/consultas/actualizar.php

--- proyecto/consultas/actualizar.php
+++ proyecto/consultas/actualizar.php
@@ -1,6 +1,6 @@
 <?php
 if(
-	!isset($_POST["nombre"]) || !isset($_POST["apellidos"]) || !isset($_POST["email"]) || !isset($_POST["pass"]) || !isset($_POST["tipo"])
+	!isset($_POST["nombre"]) || !isset($_POST["apellidos"]) || !isset($_POST["email"]) || !isset($_POST["pass"]) || !isset($_POST["tipo"]) || !isset($_POST["id"])
 ) exit();
 
 include_once "../bd/base_de_datos.php";
@@ -11,7 +11,7 @@ $sexo = $_POST["email"];
 $pass = $_POST["pass"];
 $pass = $_POST["tipo"];
 
-$sentencia = $base_de_datos->prepare("UPDATE proyecto SET nombre = ?, apellidos = ?, email = ?, pass = ?, tipo = ? WHERE id = ?;");
+$sentencia = $base_de_datos->prepare("UPDATE usuario SET nombre = ?, apellidos = ?, email = ?, pass = ?, tipo = ? WHERE id = ?;");
 $resultado = $sentencia->execute([$nombre, $apellidos, $email, $pass, $tipo, $id]);
 if($resultado === TRUE) echo "Los cambios se han guardado correctamente";
 else echo "No se ha conseguido guardar los cambios";
