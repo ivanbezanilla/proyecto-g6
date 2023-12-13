ff76699c6639671d850402e095c5a981caf9ab67
Fernandorado22 <98455338+Fernandorado22@users.noreply.github.com>
2023-12-13 18:45:41 +0100

    modified:   proyecto/consultas/eliminar.php

--- proyecto/consultas/eliminar.php
+++ proyecto/consultas/eliminar.php
@@ -2,7 +2,7 @@
 if(!isset($_GET["id"])) exit();
 $id = $_GET["id"];
 include_once "../bd/base_de_datos.php";
-$sentencia = $base_de_datos->prepare("DELETE FROM proyecto WHERE id = ?;");
+$sentencia = $base_de_datos->prepare("DELETE FROM usuario WHERE id = ?;");
 $resultado = $sentencia->execute([$id]);
 if($resultado === TRUE) echo "Eliminado correctamente";
 else echo "Algo salio mal";
