52fa26d818639554230e82cd9dbb24f0b64e88c4
Fernandorado22 <98455338+Fernandorado22@users.noreply.github.com>
2023-12-13 16:29:01 +0100

    new file:   proyecto/consultas/actualizar.php 	modified:   proyecto/Registro.html

---
+++ proyecto/consultas/actualizar.php
@@ -0,0 +1,18 @@
+<?php
+if(
+	!isset($_POST["nombre"]) || !isset($_POST["apellidos"]) || !isset($_POST["email"]) || !isset($_POST["pass"]) || !isset($_POST["tipo"])
+) exit();
+
+include_once "base_de_datos.php";
+$id = $_POST["id"];
+$nombre = $_POST["nombre"];
+$apellidos = $_POST["apellidos"];
+$sexo = $_POST["email"];
+$pass = $_POST["pass"];
+$pass = $_POST["tipo"];
+
+$sentencia = $base_de_datos->prepare("UPDATE proyecto SET nombre = ?, apellidos = ?, email = ?, pass = ?, tipo = ? WHERE id = ?;");
+$resultado = $sentencia->execute([$nombre, $apellidos, $email, $pass, $tipo, $id]);
+if($resultado === TRUE) echo "Los cambios se han guardado correctamente";
+else echo "No se ha conseguido guardar los cambios";
+?>
