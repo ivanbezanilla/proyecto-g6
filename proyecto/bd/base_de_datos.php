5d7f90aa8012baab136e048dcc9183cd033cb1e5
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-13 17:28:33 +0100

    modified:   proyecto/bd/base_de_datos.php

--- proyecto/bd/base_de_datos.php
+++ proyecto/bd/base_de_datos.php
@@ -4,7 +4,6 @@ $usuario = "grupo6";
 $nombre_base_de_datos = "PAPRM";
 try{
     $base_de_datos = new PDO('mysql:host=192.168.6.164;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
-    echo "conectado";
 }catch(Exception $e){
     echo "Ocurrio algo con la base de datos:" . $e->getMessage();
 }
