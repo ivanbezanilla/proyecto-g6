cac399d470377b4cfcc1846c3882834b07ce8647
Fernandorado22 <98455338+Fernandorado22@users.noreply.github.com>
2023-12-13 16:11:59 +0100

    modified:   proyecto/consultas/nueva.php 	modified:   proyecto/registro.html 	modified:   proyecto/Registro.html

--- proyecto/consultas/nueva.php
+++ proyecto/consultas/nueva.php
@@ -1,14 +1,15 @@
 <?php
-if(!isset($_POST["nombre"]) || !isset($_POST["apellidos"]) || !isset($_POST["dni"]) || !isset($_POST["sexo"])) exit();
+if(!isset($_POST["nombre"]) || !isset($_POST["apellidos"]) || !isset($_POST["email"]) || !isset($_POST["pass"]) || !isset($_POST["tipo"])) exit();
 
 include_once "base_de_datos.php";
 $nombre = $_POST["nombre"];
 $apellidos = $_POST["apellidos"];
-$dni = $_POST["dni"];
-$sexo = $_POST["sexo"];
+$dni = $_POST["email"];
+$pass = $_POST["pass"];
+$sexo = $_POST["tipo"];
 
-$sentencia = $base_de_datos->prepare("INSERT INTO proyecto(nombre, apellidos, dni, sexo) VALUES (?, ?, ?, ?);");
-$resultado = $sentencia->execute([$nombre, $apellidos, $dni, $sexo]);
+$sentencia = $base_de_datos->prepare("INSERT INTO proyecto(nombre, apellidos, email, pass, tipo) VALUES (?, ?, ?, ?, ?);");
+$resultado = $sentencia->execute([$nombre, $apellidos, $email, $pass, $tipo]);
 
 if($resultado === TRUE) echo "Se ha insertado correctamente";
 else echo "No se ha conseguido insertar, intentalo otra vez";
