9ecd4e030a295f2dd503e08ab0b10bc0fe586ddf
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-14 16:52:44 +0100

    a

---
+++ proyecto/consultas/listarclases.php
@@ -0,0 +1,103 @@
+<?php
+include "../bd/base_de_datos.php";
+$sentencia = $base_de_datos->query("SELECT * FROM clases;");
+$personas = $sentencia->fetchAll(PDO::FETCH_OBJ);
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Tabla de usuarios</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            height: 100vh;
+        }
+
+        header {
+            text-align: center;
+            margin-bottom: 20px; /* Ajusta el margen inferior */
+        }
+
+        table {
+            border-collapse: collapse;
+            width: 80%;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            background-color: #fff;
+        }
+
+        th, td {
+            border: 1px solid #ddd;
+            padding: 12px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        tr:hover {
+            background-color: #f5f5f5;
+        }
+
+        h1 {
+            text-align: center;
+            color: #333;
+            margin-top: 20px; /* Ajusta el margen superior */
+        }
+    </style>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="../principal.php">Inicio</a></li>
+            <li><a href="../">Perfil</a></li>
+            <li><a href="listarusuarios.php">Usuarios</a></li>
+            <li><a href="clases.php">Clases</a></li>
+        </ul>
+        <form method="post" action=""> 
+            <input type="submit" name="cerrarsesion" value="Cerrar sesion">
+        </form>
+    </nav>
+    <header>
+        <h1>Tabla de usuarios</h1>
+    </header>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Fecha</th>
+                <th>Hora</th>
+                <th>Capacidad</th>
+                <th>Profesor</th>
+                <th>Editar</th>
+                <th>Eliminar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($personas as $proyecto){ ?>
+            <tr>
+                <td><?php echo $proyecto->ID ?></td>
+                <td><?php echo $proyecto->Nombre ?></td>
+                <td><?php echo $proyecto->Fecha ?></td>
+                <td><?php echo $proyecto->Hora ?></td>
+                <td><?php echo $proyecto->Capacidad ?></td>
+                <td><?php echo $proyecto->Profesor ?></td>
+                <td><a href="<?php echo "./editar_clase.php?id=" . $proyecto->id?>">Editar</a></td>
+                <td><a href="<?php echo "./eliminar_clase.php?id=" . $proyecto->id?>">Eliminar</a></td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</body>
+</html>
