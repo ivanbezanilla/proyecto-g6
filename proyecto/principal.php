048cf48c9c3e44ad673f35d001ae147565eeba66
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-14 18:08:22 +0100

    a

--- proyecto/principal.php
+++ proyecto/principal.php
@@ -1,28 +1,7 @@
 <?php
 session_start();
 include_once "./bd/base_de_datos.php";
-/*
-//
-$username = $_POST['correo'];
-$password = $_POST['passw'];
- 
-$consulta = $base_de_datos->prepare("SELECT * FROM usuario WHERE email = :username AND pass = :password");
-$consulta->bindParam(':username', $username);
-$consulta->bindParam(':password', $password);
-$consulta->execute();
- 
-if ($consulta->rowCount() == 1) {
- 
-    $row = $consulta->fetch(PDO::FETCH_ASSOC);
-    $_SESSION['usuario'] = $row['nombre'];
- 
-} else {
-    echo "Usuario o contraseña incorrectos";
-    header("Location: index.php");
-    exit();
-}
-//
-*/
+
 if (!isset($_SESSION["usuario"])) {
     header("Location: iniciar_sesion.php");
     exit();
@@ -40,21 +19,8 @@ if (isset($_POST['cerrarsesion'])) {
 }
 
 $correoUsuario = $_SESSION['correo'];
-$tipoUsuario = $_SESSION['tipo'];
-/*
-// Consulta SQL para obtener el tipo de usuario
-$consulta = "SELECT tipo FROM usuario WHERE email = '$correoUsuario'";
-$resultado = $base_de_datos->query($consulta);
+$tipoUsuario = $_SESSION['tipousuario'];
 
-if ($resultado->rowCount() > 0) { // Utiliza rowCount() en lugar de num_rows
-    // Si se encuentra el usuario, obtén su tipo
-    $fila = $resultado->fetch(PDO::FETCH_ASSOC);
-    $tipoUsuario = $fila['tipo'];
-
-} else {
-     echo "Manejar la situación de inicio de sesión fallida";
-}
-*/
 ?>
 <!DOCTYPE html>
 <html lang="es">
