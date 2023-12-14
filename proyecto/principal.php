5d978e3b3b241be6108bd88d0b63afdc2407ac11
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-14 15:26:29 +0100

    modified:   proyecto/iniciar_sesion.php 	modified:   proyecto/principal.php

--- proyecto/principal.php
+++ proyecto/principal.php
@@ -1,6 +1,27 @@
 <?php
 session_start();
 include_once "./bd/base_de_datos.php";
+//
+$username = $_POST['correo'];
+$password = $_POST['passw'];
+ 
+$consulta = $base_de_datos->prepare("SELECT * FROM usuario WHERE email = :username AND pass = :password");
+$consulta->bindParam(':username', $username);
+$consulta->bindParam(':password', $password);
+$consulta->execute();
+ 
+if ($consulta->rowCount() == 1) {
+ 
+    $row = $consulta->fetch(PDO::FETCH_ASSOC);
+    $_SESSION['usuario'] = $row['nombre'];
+    $_SESSION['correo'] = $row['correo'];
+ 
+} else {
+    echo "Usuario o contraseña incorrectos";
+    header("Location: index.php");
+    exit();
+}
+//
 
 if (!isset($_SESSION["usuario"])) {
     header("Location: iniciar_sesion.php");
