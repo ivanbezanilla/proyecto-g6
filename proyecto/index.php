7cd17fb340b63a1f7420a42116e6e4668dd7b950
Fernandorado22 <98455338+Fernandorado22@users.noreply.github.com>
2023-12-14 16:54:02 +0100

    modified:   proyecto/index.php

--- proyecto/index.php
+++ proyecto/index.php
@@ -10,32 +10,76 @@
             margin: 0;
             padding: 0;
             background-color: #f4f4f4;
+            background-image: url('imagen.jpg');
+            background-size: cover;
+            background-attachment: fixed;
         }
 
         header {
-            background-color: #333;
+            background-color: #4CAF50;
             color: #fff;
             padding: 10px;
             text-align: center;
         }
 
         nav {
-            background-color: #444;
-            padding: 10px;
+            background-color: rgba(0, 0, 0, 0.8);
+            padding: 10px 0;
+        }
+
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
             text-align: center;
         }
 
-        nav a {
-            color: #fff;
+        nav ul li {
+            display: inline-block;
+            margin-right: 15px;
+        }
+
+        nav ul li a {
             text-decoration: none;
-            padding: 10px;
-            margin: 0 10px;
-            font-weight: bold;
+            color: #fff;
+            padding: 8px 15px;
+            border-radius: 5px;
+        }
+
+        nav ul li a:hover {
+            background-color: rgba(255, 255, 255, 0.2);
+        }
+
+        form {
+            float: right;
+            margin-top: 10px;
+            margin-right: 20px;
+        }
+
+        input[type="submit"] {
+            padding: 8px 15px;
+            border-radius: 5px;
+            background-color: #fff;
+            border: none;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
         }
 
         section {
             padding: 20px;
             text-align: center;
+        }
+
+        h2, p {
+            color: #333;
+        }
+
+        img {
+            width: 100%;
+            max-width: 600px;
         }
     </style>
 </head>
@@ -45,18 +89,24 @@
     </header>
 
     <nav>
-        <a href="#inicio">Inicio</a>
-        <a href="#cursos">Cursos</a>
-        <a href="#galeria">Galería</a>
-        <a href="#contacto">Contacto</a>
-        <a href="iniciar_sesion.php">Iniciar sesion</a>
+        <ul>
+            <?php
+            function generarMenu($tipoUsuario) {
+                // ...
+            }
+
+            echo generarMenu($tipoUsuario);
+            ?>
+        </ul>
+        <form method="post" action=""> 
+            <input type="submit" name="cerrarsesion" value="Cerrar sesión">
+        </form>
     </nav>
 
     <section id="inicio">
         <h2>Bienvenido a nuestra Academia de Pintura</h2>
         <p>Descubre el arte de la pintura con nosotros.</p>
-        <img src="imagen.jpg" alt="imagen" style="width: 100%; max-width: 600px;">
-    </section>
+        <img src="imagen.jpg" alt="imagen">
     </section>
 
     <section id="cursos">
