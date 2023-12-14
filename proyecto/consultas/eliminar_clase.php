ca0ff7de1fea5f09a44834352d8db5b36cd8126f
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-14 18:55:20 +0100

    a

---
+++ proyecto/consultas/eliminar_clase.php
@@ -0,0 +1,9 @@
+<?php
+if(!isset($_GET["id"])) exit();
+$id = $_GET["id"];
+include_once "../bd/base_de_datos.php";
+$sentencia = $base_de_datos->prepare("DELETE FROM clase WHERE id = ?;");
+$resultado = $sentencia->execute([$id]);
+if($resultado === TRUE) echo "Eliminado correctamente";
+else echo "Algo salio mal";
+?>
