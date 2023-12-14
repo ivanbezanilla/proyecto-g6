f78917194db9b1d4921efe9eaefe80eface8f5db
Fernandorado22 <98455338+Fernandorado22@users.noreply.github.com>
2023-12-14 16:33:05 +0100

    modified:   proyecto/consultas/editar.php

--- proyecto/consultas/editar.php
+++ proyecto/consultas/editar.php
@@ -78,8 +78,8 @@ if($proyecto === FALSE){
     </style>
 </head>
 <body>
-    <h1>Editar usuario</h1>
     <form method="post" action="actualizar.php">
+        <h1>Editar usuario</h1>
         <input type="hidden" name="id" value="<?php echo $proyecto->id; ?>">
         <label for="nombre">Nombre:</label>
         <input value="<?php echo $proyecto->nombre ?>" name="nombre" required type="text" id="nombre" placeholder="Escribe tu nombre">
