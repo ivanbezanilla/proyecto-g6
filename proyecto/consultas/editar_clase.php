7103f6a94c76e1522b510a62f900ef9c57ee7daf
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-14 18:42:49 +0100

    a

--- proyecto/consultas/editar_clase.php
+++ proyecto/consultas/editar_clase.php
@@ -97,8 +97,8 @@ if($proyecto === FALSE){
         <input value="<?php echo $proyecto->Hora ?>" name="hora" required type="time" id="hora">
         <label for="pass">Capacidad:</label>
         <input value="<?php echo $proyecto->Capacidad ?>" name="capacidad" required type="number" id="capacidad">
-        <label for="pass">ID Profesor:</label>
-        <input value="<?php echo $proyecto->Profesor ?>" name="profesor" required type="number" id="profesor">
+        <!--<label for="pass">ID Profesor:</label>
+        <input value="<?php echo $proyecto->Profesor ?>" name="profesor" required type="number" id="profesor">-->
         <input type="submit" value="Guardar cambios">
     </form>
 </body>
