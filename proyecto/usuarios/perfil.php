4b98e7345b4f51795c3c2b6bbcc794457420044b
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-15 19:17:39 +0100

    a

---
+++ proyecto/usuarios/perfil.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Perfil de Usuario</title>
+    <style>
+        /* Estilos CSS para la página de perfil */
+
+        /* Estilos básicos */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            width: 80%;
+            margin: 0 auto;
+            padding: 20px;
+        }
+
+        h1 {
+            text-align: center;
+        }
+
+        .profile-info {
+            border: 1px solid #ddd;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            max-width: 600px;
+            margin: 0 auto;
+        }
+
+        .profile-info p {
+            margin-bottom: 10px;
+            font-size: 16px;
+        }
+
+        .profile-info p strong {
+            font-weight: bold;
+        }
+
+        .btn {
+            display: inline-block;
+            padding: 8px 20px;
+            border-radius: 5px;
+            text-decoration: none;
+            background-color: #4CAF50;
+            color: #fff;
+            font-weight: bold;
+            transition: background-color 0.3s ease;
+        }
+
+        .btn:hover {
+            background-color: #45a049;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Perfil de Usuario</h1>
+        <div class="profile-info">
+            <p><strong>Nombre:</strong> <?php echo $nombre; ?></p>
+            <p><strong>Apellidos:</strong> <?php echo $apellidos; ?></p>
+            <p><strong>Correo electrónico:</strong> <?php echo $correo; ?></p>
+            <!-- Agrega más información del perfil si la tienes -->
+
+            <!-- Un botón para regresar a la página principal -->
+            <a href="principal.php" class="btn">Volver a la página principal</a>
+        </div>
+    </div>
+</body>
+</html>
