013c13b27d1ad0b3417b14cd4168417ed42c2265
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-15 18:39:37 +0100

    a

--- proyecto/principal.php
+++ proyecto/principal.php
@@ -101,12 +101,9 @@ $tipoUsuario = $_SESSION['tipousuario'];
     <nav>
         <ul>
             <?php
-            if (isset($_SESSION['id'])) {
-                $id = $_SESSION['id'];
-                echo "$id";
-            } else {
-                echo "El ID de usuario no está definido en la sesión.";
-            }
+            $id = $_SESSION['id'];
+            echo "$id";
+            
 
             // Lógica para generar el menú según el tipo de usuario
             function generarMenu($tipoUsuario) {
