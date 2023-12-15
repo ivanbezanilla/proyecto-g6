9b1218f12adb342eca499921afd63ef5e10379e8
Fernandorado22 <98455338+Fernandorado22@users.noreply.github.com>
2023-12-15 16:42:00 +0100

    a

--- proyecto/alumno/listar_alumno.php
+++ proyecto/alumno/listar_alumno.php
@@ -1,6 +1,6 @@
 <?php
 include "../bd/base_de_datos.php";
-$id = $_GET["id"];
+$id = $_POST["id"];
 $sentencia = $base_de_datos->prepare("SELECT * FROM clase WHERE id = ?;");
 $sentencia->execute([$id]);
 $personas = $sentencia -> fetchAll(PDO::FETCH_OBJ);
