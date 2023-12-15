e1e80d6a80a1dcf1e2b9baaecd6cebf068f8385b
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-15 18:00:21 +0100

    a

--- proyecto/usuarios/listarusuarios.php
+++ proyecto/usuarios/listarusuarios.php
@@ -143,5 +143,33 @@ $personas = $sentencia->fetchAll(PDO::FETCH_OBJ);
             <?php } ?>
         </tbody>
     </table>
+    <h1>Agregar usuario</h1>
+    <form method="post" action="aplicar_nuevo_usuario.php">
+        <fieldset>
+            <label for="nombre">Nombre:</label>
+            <br>
+            <input name="nombre" required type="text" id="nombre" placeholder="Escribe tu nombre">
+            <br><br>
+            <label for="apellidos">Apellidos:</label>
+            <br>
+            <input name="apellidos" required type="text" id="apellidos" placeholder="Escribe tus apellidos">
+            <br><br>
+            <label for="pass">Contraseña:</label>
+            <br>
+            <input name="pass" required type="password" id="pass" placeholder="Escribe tu contraseña">
+            <br><br>
+            <label for="email">Email:</label>
+            <br>
+            <input name="email" required type="text" id="email" placeholder="Escribe tu email">
+            <br><br>
+            <label for="tipo">Privilegios</label>
+            <select name="tipo" required name="tipo" id="tipo">
+                <option value="alumno">Alumno</option>
+                <option value="profesor">Profesor</option>
+                <option value="administracion">Administracion</option>
+            </select>
+            <br><br><input type="submit" value="Registrar">
+        </fieldset>
+    </form>
 </body>
 </html>
