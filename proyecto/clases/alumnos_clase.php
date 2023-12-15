aa3fe3bf759819df6816a17cfd51578487680027
ivanbezanilla <ibezanillal01@educantabria.es>
2023-12-15 19:52:45 +0100

    a

--- proyecto/clases/alumnos_clase.php
+++ proyecto/clases/alumnos_clase.php
@@ -111,9 +111,6 @@ $personas = $sentencia->fetchAll(PDO::FETCH_OBJ);
             <li><a href="../">Perfil</a></li>
             <li><a href="../usuarios/listarusuarios.php">Usuarios</a></li>
             <li><a href="listarclases.php">Clases</a></li>
-            <form method="post" action=""> 
-                <input type="submit" name="cerrarsesion" value="Cerrar sesion">
-            </form>
         </ul>
     </nav>
     
