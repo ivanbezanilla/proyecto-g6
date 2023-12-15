a3d6b18ba590d2bbe645fb7b3207324737670cb6
Fernandorado22 <98455338+Fernandorado22@users.noreply.github.com>
2023-12-15 15:46:22 +0100

    modified:   proyecto/alumno/listar_alumno.php 	modified:   proyecto/alumno/listar_alumno.php

--- proyecto/alumno/listar_alumno.php
+++ proyecto/alumno/listar_alumno.php
@@ -2,6 +2,17 @@
 include "../bd/base_de_datos.php";
 $id= 1;
 $sentencia = $base_de_datos->query("SELECT * FROM clase WHERE id = ?;");
-$personas = $sentencia->fetchAll(PDO::FETCH_OBJ);
+$sentencia -> $bindParam(':id', $id, PDO::PARAM_INT);
+$sentencia -> execute();
 
+$personas = $sentencia -> fetchAll(PDO::FETCH_OBJ);
+
+if (personas) {
+    foreach ($personas as $persona) {
+        echo "ID:" . $personas -> id . "<br>";
+        echo "Nombre:" . $persona -> nombre . "<br>";
+    }
+} else {
+    echo "No se encontraton resultados";
+}
 ?>
